c199c6ba3aef22ea2af1c893370c87df7eeaab68
diaconovi <35466672+diaconovi@users.noreply.github.com>
2018-04-14 19:26:51 -0500

    Update JUnitHelloWorld.java

--- src/main/java/junit/JUnitHelloWorld.java
+++ src/main/java/junit/JUnitHelloWorld.java
@@ -2,7 +2,12 @@ package junit;
 
 public class JUnitHelloWorld {
 
+	
 	public boolean isGreater(int num1, int num2){
 		return num1 > num2;
 	}
+
+   	public static void main(String[] args) {
+      		System.out.println("Hello, World");
+   	}
 }
